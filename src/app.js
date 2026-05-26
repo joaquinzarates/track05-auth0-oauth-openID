b38c981958c2309402cf5902117035bbdf00acc4
Joaquin Zarate Sanchez <joaquin.zarate@ids.com.mx>
2026-05-26 02:27:37 -0600

    feat: agregar archivos estáticos en app.js

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const apiRoutes  = require('./routes/apiRoutes');
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
